affe72d2e038c1b023f34a5a878502e632cca10c
liangshaojun-666 <2936670268@qq.com>
2023-07-06 19:08:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import Home from "./components/Home.vue";
+// import PageOne from "./components/PageOne.vue";
+// import PageTwo from "./components/PageTwo.vue";
+</script>
+
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/page1">Page1</router-link>
+      <router-link to="/page2">Page2</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/PageTwo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>this is page2</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
