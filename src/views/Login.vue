2618c0ab11447a557c31c6a204e2e8c651d743aa
liangshaojun-666 <2936670268@qq.com>
2023-07-13 19:38:24 +0800

    完成login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,113 @@
 <template>
-  <div>
-    <h2>njupt综合测评</h2>
-    <div>用户名<input type="text" /></div>
-    <div>密码<input type="password" name="" id="" /></div>
-
-    <button @click="go">登录</button>
+  <div class="Container">
+    <div class="box">
+      <img src="../assets/img/xueyuan.png" alt="" />
+    </div>
+    <div class="title">
+      <img src="../assets/img/right-img.png" alt="" />
+    </div>
+    <div class="mainBox">
+      <input type="text" placeholder="输入学号" />
+      <input type="password" name="" id="" placeholder="输入统一身份认证密码" />
+      <input type="submit" name="" placeholder="登录" @click="go" class="btn" />
+      <!-- <button @click="go" class="btn">登录</button> -->
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    go() {
-      this.$router.push("/home/totalassessment");
+  export default {
+    data() {
+      return {};
     },
-  },
-};
+    methods: {
+      go() {
+        this.$router.push("/home/totalassessment");
+      },
+    },
+  };
 </script>
 
 <style scoped>
+  .Container {
+    width: 100vw;
+    height: 100vh;
+    background-image: url(../assets/img/login.png);
+    background-size: 100% 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    zoom: 1;
+    z-index: -10;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .btn {
+    width: 8.75rem;
+    height: 2.875rem;
+    border-radius: 1.4375rem;
+    background-color: rgba(0, 43, 255, 1);
+    color: rgba(255, 255, 255, 1);
+    font-size: 1.15rem;
+    border: 0;
+    transition: 0.2s;
+    cursor: pointer;
+  }
+  .btn:hover {
+    width: 9rem;
+    height: 3rem;
+    background-color: #4176e9;
+  }
+  input[type="password"],
+  input[type="text"] {
+    width: 18rem;
+    height: 2.875rem;
+    opacity: 1;
+    border-radius: 0.875rem;
+    background: rgba(255, 255, 255, 1);
+    border-color: rgba(255, 255, 255, 1);
+    border: 0;
+    text-align: center;
+    outline: none;
+    caret-color: auto;
+    transition: 0.5s;
+    font-size: 1rem;
+    color: rgba(166, 166, 166, 1);
+  }
+  input[type="password"]:focus,
+  input[type="text"]:focus {
+    width: 22rem;
+    height: 3.5rem;
+    font-size: 1.5rem;
+  }
+  input:focus::-webkit-input-placeholder {
+    color: transparent;
+  }
+
+  .mainBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 6vh;
+  }
+  .box {
+    width: 20%;
+    margin-top: 15vh;
+  }
+  .box img {
+    width: 100%;
+  }
+  .title {
+    margin-top: 5vh;
+    width: 25%;
+  }
+  .title img {
+    width: 100%;
+  }
+  input[type="password"],
+  input[type="submit"] {
+    margin-top: 4vh;
+  }
 </style>
