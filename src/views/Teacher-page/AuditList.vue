78e5211ec0950fb49c2d51526c25eed3e26f0694
@aksai1111 <3501609542@qq.com>
2023-07-21 22:18:48 +0800

    完成学生列表和审核列表的大体框架，细节待完善

--- src/views/Teacher-page/AuditList.vue
+++ src/views/Teacher-page/AuditList.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="container">
-    <DatePicker></DatePicker>
-    <MajorPicker></MajorPicker>
-    <StudentClassPicker></StudentClassPicker>
-    111审核列表
+  <div class="CenterBox">
+    <div class="container">
+      <DatePicker></DatePicker>
+      <MajorPicker></MajorPicker>
+      <StudentClassPicker></StudentClassPicker>
+      111审核列表
+      <div class="whitesmall" id="ws1">
+        <img src="../../assets/img/blue.png" class="blue">
+        <div class="textBlue" id="tb1">待审核列表</div>
+      </div>
+      <div class="whitesmall" id="ws2">
+        <img src="../../assets/img/blue.png" class="blue">
+        <div class="textBlue" id="tb2">已审核</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +37,61 @@
 <style scoped>
   .CenterBox {
     margin: 0 auto;
+    width: 100vw;
+    height: 100vh;
+    background-image: url(../../assets/img/Web.png);
+    background-size: 100% 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    zoom: 1;
+    z-index: -10;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .whitesmall{
+    position: absolute;
+    top: 18.98%;
+    width: 42.8125vw;
+    height: 81.02vh;
+    opacity: 1;
+    background: rgba(255, 255, 255, 0.8);
+  }
+  #ws1{
+    position: absolute;
+    left: 6.09375%;
+  }
+  #ws2{
+    position: absolute;
+    left: 51.61%;
+  }
+  .blue{
+    position: absolute;
+    left: 4.87%;
+    top: 4.69%;
+    width: 0.21vw;
+    height: 1.76vh;
+  }
+  .textBlue{
+    position: absolute;
+    left: 6.45%;
+    top: 4.23%;
+    height: 2.50vh;
+    opacity: 1;
+    font-size: 1.04vw;
+    font-weight: 500;
+    letter-spacing: 0px;
+    line-height: 2.44vh;
+    color: rgba(0, 43, 255, 1);
+    text-align: left;
+    vertical-align: top;
+  }
+  #tb1{
+    width: 5.21vw;
+  }
+  #tb2{
+    width: 3.125vw;
   }
 </style>
--- src/views/Teacher-page/StuList.vue
+++ src/views/Teacher-page/StuList.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="container">
-    <DatePicker></DatePicker>
-    <MajorPicker></MajorPicker>
-    <StudentClassPicker></StudentClassPicker>
+  <div class="CenterBox">
+    <div class="container">
+      <DatePicker></DatePicker>
+      <MajorPicker></MajorPicker>
+      <StudentClassPicker></StudentClassPicker>
     111学生列表
+    <div class="whitelarge"></div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +30,27 @@
 <style scoped>
   .CenterBox {
     margin: 0 auto;
+    width: 100vw;
+    height: 100vh;
+    background-image: url(../../assets/img/Web.png);
+    background-size: 100% 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    zoom: 1;
+    z-index: -10;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .whitelarge{
+    position: absolute;
+    left: 9.01%;
+    top: 18.98%;
+    width: 80.36vw;
+    height: 81.02vh;
+    opacity: 1;
+    background: rgba(255, 255, 255, 0.8);
   }
 </style>
